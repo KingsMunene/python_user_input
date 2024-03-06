2763d1b812eff76f1b6a0c74f8f8559de7d97895
KingsMunene <munenekings2@gmail.com>
2024-03-06 14:14:12 +0300

    Added comments to the file

--- user_input.py
+++ user_input.py
@@ -1,4 +1,9 @@
+#Ask the user for a name using the input method
 name = input("Whats your name: ")
+
+#Ask the user to input their age using the input method
 age = input("What is your age: ")
+
+#Ask the user for location using the input method
 location = input("Where do you come from?: ")
 print("Hello " + name + ", you are " + age + " years old and live in " + location)
