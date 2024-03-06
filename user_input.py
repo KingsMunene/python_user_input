9d6ceb83322be618e6e5acbf28f9b84b62359ba5
KingsMunene <munenekings2@gmail.com>
2024-03-06 14:09:39 +0300

    Created a file user_input.py and added the code

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Whats your name: ")
+age = input("What is your age: ")
+location = input("Where do you come from?: ")
+print("Hello " + name + ", you are " + age + " years old and live in " + location)
